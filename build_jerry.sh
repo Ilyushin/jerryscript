3dbcbd79cb901b0d4ff4a1de127c4f04de4cd495
Ilyushin <jon.ilyushin@gmail.com>
2015-08-14 08:58:44 +0300

    Add script for building

---
+++ build_jerry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ./jr_ilyushin/jerryscript
+LTO=OFF USE_COMPILER_DEFAULT_LIBC=YES make debug.linux -j
+
---
+++ run.py
@@ -0,0 +1,5 @@
+import sys
+
+if len(sys.argv) == 2:
+    #set parameters
+    path_test = os.path.abspath(sys.argv[1])
